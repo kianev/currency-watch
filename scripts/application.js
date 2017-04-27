a8bed83f99329896bec81f4deb2223aa73bc7125
Kaloian Ianev <kianev@gmail.com>
2017-04-27 18:15:35 +0300

    Parsed JSON data from server, added basic structure of currency table, formated currency output.

--- scripts/application.js
+++ scripts/application.js
@@ -1,1 +1,8 @@
-console.log("script loaded");
+
+let ratesJSON = $.get("https://openexchangerates.org/api/latest.json",{app_id: "282beb85831a4aadad7086809d54e5fb"},
+    function (data) {$("#eur").append((1/(data.rates.EUR)).toFixed(4));
+                     $("#gbp").append((1/(data.rates.GBP)).toFixed(4));
+                     $("#jpy").append((data.rates.JPY).toFixed(4));
+                     $("#aud").append((data.rates.AUD).toFixed(4));
+                     $("#bgn").append((data.rates.BGN).toFixed(4))});
+
