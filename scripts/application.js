a3cce5a4315271eb8cf9b20149554af19c9cf8a0
Kaloian Ianev <kianev@gmail.com>
2017-05-12 12:36:51 +0300

    refactoring

--- scripts/application.js
+++ scripts/application.js
@@ -1,3 +1,10 @@
+/**
+ * 1. Get initial FX Rates from JSON
+ * 2. update HTML Table with the data
+ * 3. update FX Rates JSON and comparison with the previous FX Ratas
+ * 4. get the color codes for movement and arrow signals
+ */
+
 const appID = "282beb85831a4aadad7086809d54e5fb";
 const openExchangeRatesURL = "https://openexchangerates.org/api/latest.json";
 
@@ -5,7 +12,7 @@ function getInitialData() {
     $.getJSON(openExchangeRatesURL, {app_id: appID},
         function (data) {
             let currInit = [data.rates.USD, data.rates.EUR, data.rates.GBP, data.rates.JPY, data.rates.AUD, data.rates.BGN];
-
+                //update initial HTML table with rates
             for (let i = 1; i < currInit.length; i++) {
                 $("#" + i).append((currInit[i]).toFixed(4));
             }
@@ -15,9 +22,9 @@ function getInitialData() {
 setInterval(function updateData() {
 
     for (let i = 1; i < 6; i++) {
-
+       //comparison initial rates
         let cur = Number($("#" + i).html());
-
+       //get the new rates
         $.getJSON(openExchangeRatesURL, {app_id: appID},
             function (data) {
                 let currUpdate = [data.rates.USD, data.rates.EUR, data.rates.GBP, data.rates.JPY, data.rates.AUD, data.rates.BGN];
@@ -57,6 +64,7 @@ function getRandomFXRates(min, max) {
  }
 
  getData(openExchangeRatesURL,appID).done(function (newData) {
+
     //console.log(newData);
  });
 
